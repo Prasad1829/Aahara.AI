328ef962284abcc52e1bce293cd649386bda5687
rutika0103 <sarjerutika01@gmail.com>
2026-03-11 16:05:14 +0530

    Delete accounts directory

--- accounts/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Recipe
-
-@admin.register(Recipe)
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'cooking_time')
-# -------------------------------------------------------------------------------------------------------
-
-
